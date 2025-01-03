08c740f8fccd70694950910551ea219f0e3d6f5f
JonnyElkinDfT <jonny.elkin@dft.gov.uk>
2025-01-03 15:23:31 +0000

    add seperate backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket = "tf-state-for-go-reply-sandbox" # Replace with your bucket name
-    prefix = "workload-identity-testing/"
+    prefix = "github-pipeline-test-state/"
   }
 }
